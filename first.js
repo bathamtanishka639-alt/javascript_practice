97def6650c9fdc76a00d302919f1c467b6b6853e
bathamtanishka639 <bathamtanishka639@gmail.com>
2026-07-22 22:13:07 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,27 @@
+console.log("hello world");
+console.log("Today I start my javascript learning");
+console.log("And I am very happy to start my learning journey again");
+fullname="Tanishka";
+console.log(fullname);
+isfollow=true;
+console.log(isfollow);
+let age=25;
+age=26;
+console.log(age);
+const price=15;
+console.log(price);
+let x=BigInt("123");
+console.log(x);
+let y=Symbol("hello");
+console.log(y);
+const student= {
+    rollno:2,
+    marks:75,
+    clg:Abes,
+};
+student["marks"]=student["marks"] +1;
+console.log(student.marks);
+console.log(student["marks"]);
+console.log(typeof fullname);
+// this is a single line comment
+/* this is a multi line comment */
---
+++ script.js
@@ -0,0 +1,28 @@
+// learn about operators
+let a=8;
+let b=4;
+a++;
+b--;
+console.log(b);
+console.log(a);
+console.log("a+b=",a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+//alert is a pop up which display some warning message on browser. and it is one time pop up
+alert("hello");
+//prompt is also display a message on browser but  take input from the user also
+prompt("hello");
+let name=prompt("hello");
+
+// question get user to input a number using prompt ("enter a number") and check the number is a multiple of 5 or not
+let number=prompt("number");
+
+if(number%5==0){
+    console.log("yes");
+}else{
+    console.log("no");
+}
+
+
