93f76f873a952088849c7e1b3d858d692869b168
Tanishka Batham <bathamtanishka639@gmail.com>
2026-07-30 00:43:50 +0530

    Add files via upload
    learning Document Object Model Basic

---
+++ Dom.js
@@ -0,0 +1,8 @@
+console.log("hello");
+alert("hi");
+console.log(window);
+window.console.log("li");//window is a global function that the javascript body know about it 
+//to acess html in js
+console.dir(document);
+//to acess body of html
+console.dir(document.body);
