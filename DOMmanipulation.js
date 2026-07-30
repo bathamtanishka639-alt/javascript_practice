e807087839cf5144e9839b27ce8171f96dfd66af
Tanishka Batham <bathamtanishka639@gmail.com>
2026-07-30 20:20:51 +0530

    Add files via upload
    DOM modulation

---
+++ DOMmanipulation.js
@@ -0,0 +1,14 @@
+let heading=document.getElementById("heading");
+console.log(heading);
+console.dir(heading);
+let heading5=document.getElementsByClassName("heading5");
+console.log(heading5);
+console.dir(heading5);
+//to acess all first element in tag use query selector
+let first_element=document.querySelector("p");
+console.dir(first_element);
+//return first element
+//to acess all element
+let all=document.querySelectorAll("p");
+// it return nodelist
+console.dir(all);
